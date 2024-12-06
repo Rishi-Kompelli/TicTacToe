4b1365c73b43a5262cde521cc471408dd103c66f
Rishi-Kompelli <1108647@apps.nsd.org>
2024-12-06 06:45:20 +0000

    added board and printboard

--- TicTacToe.java
+++ TicTacToe.java
@@ -17,14 +17,28 @@ public class TicTacToe {
         Player player2 = new Player(player2Name, player2Symbol);
         System.out.println(player2.toString());
 
+        Player activePlayer = player1;
+
         String board[][] = {
-            {" ", " ", " "},
-            {" ", " ", " "},
-            {" ", " ", " "}
+            {"-", "-", "-"},
+            {"-", "-", "-"},
+            {"-", "-", "-"}
         };
 
-        
+        /* 
+        create a turn system
+        be able to write on the board on your turn
+        */
+        printBoard(board);
 
         scanner.close();
+    }
+    public static void printBoard(String[][] board) {
+        for (int i = 0; i<3; i++){
+            System.out.println();
+            for (int j = 0; j<3; j++) {
+                System.out.print(board[i][j]+" ");
+            }
+        }
     }
 }
